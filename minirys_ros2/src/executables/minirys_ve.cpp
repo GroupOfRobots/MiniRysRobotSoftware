4948491366ef209f7b4945d1af8235d535336555
Kubaless <kuba.ostrysz@gmail.com>
2024-02-06 20:25:04 +0100

    Lidar and lidar servo integration

--- minirys_ros2/src/executables/minirys_ve.cpp
+++ minirys_ros2/src/executables/minirys_ve.cpp
@@ -1,6 +1,6 @@
 #include "minirys_ros2/helpers/RTTExecutor.hpp"
 #include "minirys_ros2/nodes/FanNode.hpp"
-//#include "minirys_ros2/nodes/MotorsNode.hpp"
+// #include "minirys_ros2/nodes/MotorsNode.hpp"
 #include "minirys_ros2/nodes/ServoNode.hpp"
 
 int main(int argc, char const* argv[]) {
@@ -13,11 +13,11 @@ int main(int argc, char const* argv[]) {
 
 	auto fanNode = FanNode::make_shared(options);
 	auto servoNode = ServoNode::make_shared(options);
-	//auto motorsNode = MotorsNode::make_shared(options);
+	// auto motorsNode = MotorsNode::make_shared(options);
 
 	executor.add_node(fanNode);
 	executor.add_node(servoNode);
-	//executor.add_node(motorsNode);
+	// executor.add_node(motorsNode);
 
 	executor.spin();
 	rclcpp::shutdown();
