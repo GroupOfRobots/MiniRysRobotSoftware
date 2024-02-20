3d245262ca20d1976ca704ef616fbd0d7a527624
Kubaless <kuba.ostrysz@gmail.com>
2024-02-20 15:47:51 +0100

    Clean up after integration with wiringPi library

--- minirys_ros2/src/executables/motors.cpp
+++ minirys_ros2/src/executables/motors.cpp
@@ -3,11 +3,6 @@
 
 int main(int argc, char const* argv[]) {
 	RTTExecutor::setupRT(3, 98, SCHED_RR);
-    // initiate communication interfaces
-    // if (wiringPiSetup() == -1) {
-    //     fprintf(stderr, "Not able to init the wiringPi library\n");
-    //     return -1;
-    // }
 	rclcpp::init(argc, argv);
 
 	rclcpp::executors::SingleThreadedExecutor executor;
