1264cbfe1b49f0bbeef41cc125ad7f7fa041bd9a
Jsadows <j.sadowski09@gmail.com>
2024-08-01 16:46:42 +0200

    adding yolov7 and wights

---
+++ shuttlecock_detect/yolov7_time_test.cpp
@@ -0,0 +1,34 @@
+#include "yolo.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <chrono>
+
+YoloV7 yolov7;
+int target_size = 420;
+
+int main(int argc, char** argv)
+{
+    yolov7.load(target_size, "best100ep1inp.ncnn.param", "best100ep1inp.ncnn.bin");
+    cv::Mat m = cv::imread("/home/Jakub/Documents/test/image_29_1717764171-923069_jpg.rf.d3d04ad129cfd8cdbcc8a5b3e8ccea9f.jpg", 1);
+    if (m.empty())
+    {
+        fprintf(stderr, "cv::imread failed\n");
+        return -1;
+    }
+    std::vector<Object> objects;
+    auto beg = std::chrono::high_resolution_clock::now();
+    yolov7.detect(m, objects, 0.1f, 0.1f);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg);
+    std::cout<< duration.count() <<std::endl;
+    std::cout<< objects.size() << std::endl;
+    yolov7.draw(m, objects);
+    cv::imshow("RPi4 - 1.95 GHz - 2 GB ram",m);
+//    cv::imwrite("test.jpg",m);
+    cv::waitKey(0);
+    return 0;
+}
