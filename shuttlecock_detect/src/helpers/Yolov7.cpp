aae85e1aab8cd9061d78a1dc14f3fcac0f58eaea
Jakub Sadowski <116253852+Jsadows@users.noreply.github.com>
2024-08-22 22:40:21 +0200

    Update Yolov7 labels

--- shuttlecock_detect/src/helpers/Yolov7.cpp
+++ shuttlecock_detect/src/helpers/Yolov7.cpp
@@ -302,23 +302,7 @@ int YoloV7::detect(const cv::Mat &rgb, std::vector<Object> &objects, float prob_
 
 int YoloV7::draw(cv::Mat &rgb, const std::vector<Object> &objects) {
     static const char *class_names[] = {
-            "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
-            "traffic light",
-            "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse",
-            "sheep", "cow",
-            "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie",
-            "suitcase", "frisbee",
-            "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove",
-            "skateboard", "surfboard",
-            "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl",
-            "banana", "apple",
-            "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake",
-            "chair", "couch",
-            "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote",
-            "keyboard", "cell phone",
-            "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase",
-            "scissors", "teddy bear",
-            "hair drier", "toothbrush"
+            "shuttlecock_back", "shuttlecock_front", "shuttlecock_side"
     };
 
     for (size_t i = 0; i < objects.size(); i++) {
