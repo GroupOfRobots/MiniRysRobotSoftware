54764ca7aea779c866a69a3c9ef211b6be6b4f56
Jakub Sadowski <you@example.com>
2024-09-04 22:41:01 +0200

    adding goal publisher to detection programm

--- shuttlecock_detect/include/shuttlecock_detect/nodes/DetectorNode.hpp
+++ shuttlecock_detect/include/shuttlecock_detect/nodes/DetectorNode.hpp
@@ -11,13 +11,17 @@
 #include "geometry_msgs/msg/twist.hpp"
 #include <geometry_msgs/msg/pose_stamped.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
+#include "nav_msgs/msg/odometry.hpp"
+#include <utility>
 #include <iostream>
+#include <math.h>
 
 using namespace std::chrono_literals;
 
 enum DetectorStates{
 	DETECTING,
     GETTING_CLOSER,
+    WAITING_FOR_ARRIVAL,
     DOCKING,
     GETTING_BACK
 };
@@ -30,11 +34,14 @@ class Detector: public rclcpp::Node{
     private:
 
     void timer_callback();
-    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg); 
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg);
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+    std::pair<float, float> calculate_dist();
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_detected_;
-    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_goal_;
+    rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr publisher_goal_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_image_;
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_odom_;
 
     cv::Mat ori_img_;
     cv::Mat detected_img_;
@@ -44,4 +51,9 @@ class Detector: public rclcpp::Node{
     float focal_length_;
     float width_side_;
     float width_front_;
+    nav_msgs::msg::Odometry current_odom_;
+    DetectorStates state_;
+    int counter_;
+    float ori_dist_;
+    bool is_closer_;
 };
--- shuttlecock_detect/src/nodes/DetectorNode.cpp
+++ shuttlecock_detect/src/nodes/DetectorNode.cpp
@@ -41,39 +41,80 @@ Detector::Detector() : Node("detector")
     RCLCPP_INFO_STREAM(this->get_logger(), "Got param: timer_period " << timer_period);
 
     //publishers
-    publisher_detected_ =
-    this->create_publisher<sensor_msgs::msg::Image>("/img_detect", 10);
+    publisher_detected_ = this->create_publisher<sensor_msgs::msg::Image>("/img_detect", 10);
     timer_ = this->create_wall_timer(std::chrono::duration<double>(timer_period), std::bind(&Detector::timer_callback, this));
+    publisher_goal_= this->create_publisher<geometry_msgs::msg::PoseStamped>("/goal_pose", 10);
+    
     //subscribers
     subscription_image_ = this->create_subscription<sensor_msgs::msg::Image>(
         "/cv_video_frames_plain_img", 10, std::bind(&Detector::image_callback, this, std::placeholders::_1));
+    subscription_odom_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        "odom", 10, std::bind(&Detector::odom_callback, this, std::placeholders::_1));
+
+    state_ = DETECTING;
+    counter_ = 0;
+    is_closer_ = false;
+    ori_dist_ = 0;
+    // TODO rozpocznij krążenie
 }
 
 void Detector::timer_callback() 
 {   
     if( !ori_img_.empty() )
     {
-        detected_img_ = ori_img_;
-        std::vector<Object> objects;
-        yolov7_->detect(detected_img_, objects, prob_threshold_, nms_threshold_);
-        yolov7_->draw(detected_img_, objects);
-        auto msg_img = cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", detected_img_).toImageMsg();
-        publisher_detected_->publish(*msg_img);
-        float dist;
-        float deltX;
-        float focal_px = focal_length_ * (float)detected_img_.size().width/3.6;
-        if(objects.size() >0)
+        if(state_ == DETECTING)
         {
-            if( objects[0].label == 0 )
+            std::pair<float, float> distances = calculate_dist();
+            if(distances.first != -1.0)
             {
-                dist = (width_side_ * focal_px)/ objects[0].rect.width;
+                RCLCPP_INFO_STREAM(this->get_logger(), "dist: "<< distances.first<<" x: "<<distances.second );
+                state_ = GETTING_CLOSER;
+                ori_dist_ = distances.first;
+                // TODO Push Goal
+                float angle = std::atan2(2.0*(current_odom_.pose.pose.orientation.w * current_odom_.pose.pose.orientation.z + current_odom_.pose.pose.orientation.x * current_odom_.pose.pose.orientation.y),
+                    1.0-2.0*(current_odom_.pose.pose.orientation.y*current_odom_.pose.pose.orientation.y + current_odom_.pose.pose.orientation.z * current_odom_.pose.pose.orientation.z) );
+                float new_x = current_odom_.pose.pose.position.x + distances.first * std::cos(angle) -distances.second * std::sin(angle); 
+                float new_y = current_odom_.pose.pose.position.y + distances.first * std::sin(angle) +distances.second * std::cos(angle);
+                auto msg = geometry_msgs::msg::PoseStamped();
+                msg.header.frame_id = "map";
+                msg.header.stamp = this->get_clock()->now();
+                msg.pose.position.x = new_x;
+                msg.pose.position.y = new_y;
+                msg.pose.position.z = current_odom_.pose.pose.position.z;
+                msg.pose.orientation = current_odom_.pose.pose.orientation;
+                publisher_goal_->publish(msg);
             }
-            else
+        }
+        else if(state_ == GETTING_CLOSER)
+        {
+            ++counter_;
+            std::pair<float, float> distances = calculate_dist();
+            if(distances.first != -1.0)
             {
-                dist = (width_front_ * focal_px)/ objects[0].rect.width;
+                RCLCPP_INFO_STREAM(this->get_logger(), "dist: "<< distances.first<<" x: "<<distances.second );
+                if(ori_dist_ >= distances.first && counter_ > 1) 
+                {
+                    is_closer_ = true;
+                }
             }
-            deltX = dist/focal_length_ *(3.6/detected_img_.size().width) * ( (float)objects[0].rect.x + (float)objects[0].rect.width/2.0 - (float)detected_img_.size().width/2.0);
-            RCLCPP_INFO_STREAM(this->get_logger(), "dist: "<< dist<<" x: "<<deltX );
+            if(counter_ == 4)
+            {
+                if(is_closer_)
+                {
+                    state_ = WAITING_FOR_ARRIVAL;
+                }
+                else
+                {
+                    state_ = DETECTING;
+                    // TODO przestań jechać do celu i zacznij szukać
+                }
+                counter_ = 0;
+                is_closer_ = 0;
+            }
+        }
+        else if(state_ == WAITING_FOR_ARRIVAL)
+        {
+            //TODO oczekiwanie na akcje
         }
     }
 }
@@ -83,3 +124,41 @@ void Detector::image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
     ori_img_ = cv_bridge::toCvCopy(msg, "rgb8")->image;
 }
 
+void Detector::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+	auto position = msg->pose.pose.position;
+	auto orientation = msg->pose.pose.orientation;
+	RCLCPP_INFO(this->get_logger(), "Position -> x: %f, y: %f, z: %f", position.x, position.y, position.z);
+	RCLCPP_INFO(this->get_logger(), "Orientation -> x: %f, y: %f, z: %f, w: %f",
+            orientation.x, orientation.y, orientation.z, orientation.w);
+    current_odom_.pose.pose.position = msg->pose.pose.position;
+    current_odom_.pose.pose.orientation = msg->pose.pose.orientation;
+}
+
+std::pair<float, float> Detector::calculate_dist()
+{
+    detected_img_ = ori_img_;
+    std::vector<Object> objects;
+    yolov7_->detect(detected_img_, objects, prob_threshold_, nms_threshold_);
+    yolov7_->draw(detected_img_, objects);
+    auto msg_img = cv_bridge::CvImage(std_msgs::msg::Header(), "rgb8", detected_img_).toImageMsg();
+    publisher_detected_->publish(*msg_img);
+    float dist;
+    float deltX;
+    float focal_px = focal_length_ * (float)detected_img_.size().width/3.6;
+    if(objects.size() >0)
+    {
+        if( objects[0].label == 0 )
+        {
+            dist = (width_side_ * focal_px)/ objects[0].rect.width;
+        }
+        else
+        {
+            dist = (width_front_ * focal_px)/ objects[0].rect.width;
+        }
+        deltX = dist/focal_length_ *(3.6/detected_img_.size().width) * 
+            ( (float)objects[0].rect.x + (float)objects[0].rect.width/2.0 - (float)detected_img_.size().width/2.0);
+        return std::make_pair(dist, deltX );
+    }
+    return std::make_pair(-1.0, -1.0);
+}
