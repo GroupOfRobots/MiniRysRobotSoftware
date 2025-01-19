dee285c665c08aa24672d4bd52a5e40bdb8d4eb7
Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>
2025-01-19 15:36:33 +0100

    Refactor followers launch files
    Remove trailing whitespace and add sensible formatting
    
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>

--- followers/launch/combined_follower.launch.py
+++ followers/launch/combined_follower.launch.py
@@ -1,41 +1,43 @@
 import os
 from ament_index_python.packages import get_package_share_directory
-from launch import actions, launch_description_sources, LaunchDescription,  substitutions
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
-
     config = os.path.join(
         get_package_share_directory('followers'),
         'config',
         'params.yaml'
-        )
+    )
 
-    wall_f = actions.IncludeLaunchDescription(
-		launch_description_sources.PythonLaunchDescriptionSource(
-			os.path.join(
-				get_package_share_directory('followers'),
-				'launch/wall_follower.launch.py')
-		)
-	)
-	
-    line_f = actions.IncludeLaunchDescription(
-		launch_description_sources.PythonLaunchDescriptionSource(
-			os.path.join(
-				get_package_share_directory('followers'),
-				'launch/line_follower.launch.py')
-		)
-	)
-        
-    namespace_value = substitutions.LaunchConfiguration('minirys_namespace')
-    arg_namespace = actions.DeclareLaunchArgument('minirys_namespace', default_value='minirys')
+    wall_f = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('followers'),
+                'launch/wall_follower.launch.py'
+            )
+        )
+    )
+
+    line_f = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory('followers'),
+                'launch/line_follower.launch.py'
+            )
+        )
+    )
+
+    namespace_value = LaunchConfiguration('minirys_namespace')
+    arg_namespace = DeclareLaunchArgument('minirys_namespace', default_value='minirys')
     node=Node(
         package = 'followers',
         name = 'combined_follower',
         executable = 'combinedFollower',
-        parameters = [config, {'minirys_namespace': namespace_value}],
-        
+        parameters = [config, {'minirys_namespace': namespace_value}]
     )
 
-    ld = LaunchDescription([arg_namespace, line_f, node, wall_f])
-    return ld
+    return LaunchDescription([arg_namespace, line_f, node, wall_f])
