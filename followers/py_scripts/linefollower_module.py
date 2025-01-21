c55afa57a7f9d1a6cd0f4fc58df96f98df57891a
Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>
2025-01-21 23:04:49 +0100

    Refactor line follower common module
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>

--- followers/py_scripts/linefollower_module.py
+++ followers/py_scripts/linefollower_module.py
@@ -2,6 +2,7 @@ import cv2 as cv
 import numpy as np
 from pid import PID
 from rclpy.impl.rcutils_logger import RcutilsLogger
+from rclpy.logging import get_logger
 from enum import Enum
 from typing import Optional
 
@@ -14,14 +15,14 @@ class LineFollowerModule:
         RAW = 1
         PROCESSED = 2
 
-    _image = None
-    _input_mode = InputMode.NONE
-    _image_binary = None
-    _pid = None
-    #_control_points = [(220, 270),(220, 230),(220, 200),(220, 170)]
-    _control_points = [(220, 200)]
-    _leftT = (435, 270)
-    _rightT = (5, 270)
+    image_ = None
+    input_mode_ = InputMode.NONE
+    image_binary_ = None
+    pid_ = None
+    #control_points_ = [(220, 270),(220, 230),(220, 200),(220, 170)]
+    control_points_ = [(220, 200)]
+    leftT_ = (435, 270)
+    rightT_ = (5, 270)
 
     def __init__(
             self,
@@ -32,19 +33,20 @@ class LineFollowerModule:
             min_u: float,
             max_u: float,
             turn_offset: float,
-            logger: Optional[RcutilsLogger]):
-        self._pid = PID(step_time, pid_k, pid_ti, pid_td)
-        self._min_u = min_u
-        self._max_u = max_u
-        self._turn_offset = turn_offset
-        self._logger = logger
+            logger: Optional[RcutilsLogger]
+        ) -> None:
+        self.pid_ = PID(step_time, pid_k, pid_ti, pid_td)
+        self.min_u_ = min_u
+        self.max_u_ = max_u
+        self.turn_offset_ = turn_offset
+        self.logger_ = logger if logger is not None else get_logger("LineFollowerModule")
 
-    def set_image(self, img: np.ndarray, mode: InputMode) -> None:
+    def set_image(self, image: np.ndarray, mode: InputMode) -> None:
         """
         Image should be either binary or encoded in RGB
         """
-        self._image = img
-        self._input_mode = mode
+        self.image_ = image
+        self.input_mode_ = mode
 
     def compute_angular_velocity(self) -> Optional[tuple[float, np.ndarray]]:
         try:
@@ -52,71 +54,74 @@ class LineFollowerModule:
             contour = self.__compute_max_contour()
 
         except LineFollowerModuleError as e:
-            self.__warning(f"Encountered error \"{e}\"."
-                           + " Errors at this stage can be raised due to the input image."
-                           + " Skipping this update!")
+            self.logger_.warning(f"Encountered error \"{e}\"."
+                                 + " Errors at this stage can be raised due to the input image."
+                                 + " Skipping this update!")
             return None
 
-        debug_image = self._image
+        debug_image = self.image_
         cv.drawContours(image=debug_image, contours=[contour], contourIdx=-1, color=(0, 255, 0), thickness=3, lineType=cv.LINE_AA)
 
         turn_offset = 0
-        sum_dist = 0
-        for control_point in self._control_points:
-            x_value = 0
-            y_value = control_point[1]
-            i = 0
+        error_sum = 0
+        for control_point in self.control_points_:
+            matching_sum = 0
+            matching_count = 0
             for contour_point in contour:
-                contour_point = contour_point[0]
+                contour_point = contour_point[0]  # This is necessary due to the way OpenCV works
 
-                if contour_point[1] == y_value:
-                    x_value += contour_point[0]
-                    i += 1
+                # Increment when vertical coordinate matches
+                if contour_point[1] == control_point[1]:
+                    matching_sum += contour_point[0]
+                    matching_count += 1
 
-            if i != 0:
-                sum_dist += x_value/i - control_point[0]
-                cv.circle(debug_image, control_point,             radius=5, color=(0, 0, 255), thickness=-1)
-                cv.circle(debug_image, (int(x_value/i), y_value), radius=5, color=(255, 0, 0), thickness=-1)
+            if matching_count != 0:
+                matching_mean = matching_sum / matching_count
+                error_sum += matching_mean - control_point[0]
 
-        y = sum_dist / len(self._control_points)
+                cv.circle(debug_image, control_point,                          radius=5, color=(0, 0, 255), thickness=-1)
+                cv.circle(debug_image, (int(matching_mean), control_point[1]), radius=5, color=(255, 0, 0), thickness=-1)
+
+        error_mean = error_sum / len(self.control_points_)
 
         # recognizing 90 degree turn
-        if cv.pointPolygonTest(contour, self._leftT, False) >= 0 and cv.pointPolygonTest(contour, self._rightT, False) < 0 :
-            turn_offset = -self._turn_offset#-1.0 #-1
+        if self.__is_in_polygon(contour, self.leftT_) and not self.__is_in_polygon(contour, self.rightT_):
+            turn_offset = -self.turn_offset_#-1.0 #-1
 
-        elif cv.pointPolygonTest(contour, self._leftT, False) < 0 and cv.pointPolygonTest(contour, self._rightT, False) >= 0 :
-            turn_offset = self._turn_offset#1.0 #1
+        elif not self.__is_in_polygon(contour, self.leftT_) and self.__is_in_polygon(contour, self.rightT_):
+            turn_offset = self.turn_offset_#1.0 #1
 
-        u = self._pid.pid(y, 0) + turn_offset
-        u = np.clip(u, self._min_u, self._max_u)
-
-
+        u = self.pid_.pid(error_mean, 0) + turn_offset
+        u = np.clip(u, self.min_u_, self.max_u_)
         return u, debug_image
 
+    def __is_in_polygon(self, polygon: np.ndarray, point: tuple) -> bool:
+        return cv.pointPolygonTest(polygon, point, False) >= 0
+
     def __process_image(self) -> None:
-        if self._image is None:
+        if self.image_ is None:
             raise LineFollowerModuleError('Cannot process image when the image is not set')
 
-        if self.InputMode.PROCESSED == self._input_mode:
-            self._image_binary = self._image
+        if self.InputMode.PROCESSED == self.input_mode_:
+            self.image_binary_ = self.image_
             return
 
-        elif self.InputMode.RAW == self._input_mode:
-            gray = cv.cvtColor(self._image, cv.COLOR_BGR2GRAY)
+        elif self.InputMode.RAW == self.input_mode_:
+            gray = cv.cvtColor(self.image_, cv.COLOR_BGR2GRAY)
             kernel = np.ones((5, 5), np.uint8)
             img_erosion = cv.erode(gray, kernel, iterations=1)
 
             _, thresh = cv.threshold(img_erosion, 127, 255, cv.THRESH_BINARY_INV)
-            self._image_binary = thresh
+            self.image_binary_ = thresh
             return
 
         raise LineFollowerModuleError('Cannot process image when input mode is not set')
 
     def __compute_max_contour(self) -> np.ndarray:
-        if self._image_binary is None:
+        if self.image_binary_ is None:
             raise LineFollowerModuleError('Cannot compute max contour when the image is not set')
 
-        contours, _ = cv.findContours(image=self._image_binary, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)
+        contours, _ = cv.findContours(image=self.image_binary_, mode=cv.RETR_EXTERNAL, method=cv.CHAIN_APPROX_NONE)
         areas = [cv.contourArea(c) for c in contours]
 
         if 0 == np.size(areas):
@@ -124,15 +129,3 @@ class LineFollowerModule:
 
         max_idx = np.argmax(areas)
         return contours[max_idx]
-
-    def __info(self, message: str) -> None:
-        if self._logger is not None:
-            self._logger.info(message)
-        else:
-            print("INFO:", message)
-
-    def __warning(self, message: str) -> None:
-        if self._logger is not None:
-            self._logger.warning(message)
-        else:
-            print("WARNING:", message)
