20d966d21abb15c4a6e25da21112f1d222df6376
Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>
2025-01-21 23:51:13 +0100

    Modify line follower nodes to use LineFollowerModule
    Signed-off-by: Mateusz Palczuk <mateusz.palczuk.stud@pw.edu.pl>

--- followers/py_scripts/linefollower.py
+++ followers/py_scripts/linefollower.py
@@ -4,23 +4,16 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
-import numpy as np
 from picamera2 import Picamera2
-from pid import PID
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
 from rclpy.parameter import Parameter
+from linefollower_module import LineFollowerModule, LineFollowerModuleError
 
 class LineFollower(Node):
-
-    img = None
+    bridge = CvBridge()
+    is_working = True
     picam2 = None
-    pid = None
-    #points = [(220, 270),(220, 230),(220, 200),(220, 170)]
-    points = [(220, 200)]
-    leftT = (435, 270)
-    rightT = (5, 270)
-    y = 0
 
     def __init__(self):
         super().__init__('line_follower')
@@ -37,7 +30,6 @@ class LineFollower(Node):
                 ('linearSpeed', Parameter.Type.DOUBLE)
             ]
         )
-
         self.maxU = self.get_parameter("maxU").get_parameter_value().double_value
         self.minU = -self.maxU
         self.speed = self.get_parameter("linearSpeed").get_parameter_value().double_value
@@ -49,6 +41,8 @@ class LineFollower(Node):
 
         self.logger.info(f"timer_period: { timer_period}, maxU: {self.maxU}, K: {K}, Ti:{Ti}")
         self.logger.info(f"Td: {Td},turnOffsetParam: {self.turnOffsetParam}, speed: {self.speed}, namespace:{self.get_namespace()}")
+
+        self.line_follower = LineFollowerModule(timer_period, K, Ti, Td, self.minU, self.maxU, self.turnOffsetParam, self.get_logger())
 
         # Publishers
         self.publisher1_ = self.create_publisher(Image, 'binn_img', 10)
@@ -65,82 +59,39 @@ class LineFollower(Node):
         config = self.picam2.create_preview_configuration(lores={"size": (640,480)})
         self.picam2.configure(config)
         self.picam2.start()
-        # PID
-        self.pid = PID(timer_period,K,Ti,Td)
-        self.is_working = True
         #self.logger.info(f"end")
-
-
 
     def timer_callback(self):
         #self.logger.info(f"running{self.isWorking}")
-        if self.is_working:
-            yuv = self.picam2.capture_array("lores")
-            self.img = cv2.cvtColor(yuv, cv2.COLOR_YUV420p2RGB)[0:450, 100:540]
-            gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
-            kernel = np.ones((5, 5), np.uint8)
-            img_erosion = cv2.erode(gray, kernel, iterations=1)
-            ret,thresh = cv2.threshold(img_erosion, 127, 255, cv2.THRESH_BINARY_INV)
-            #Negative binn
-            #thresh = cv2.bitwise_not(thresh)
-            br = CvBridge()
-            msg = br.cv2_to_imgmsg(thresh)
-            self.publisher1_.publish(msg)
-            # contours
-            contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-            img2 = self.img
-            # biggest contour
-            areas = [cv2.contourArea(c) for c in contours]
+        if not self.is_working:
+            return
 
-            # getting position where robot should be
-            turnOfset = 0
-            if not np.size(areas) == 0:
-                max_index = np.argmax(areas)
-                cnt=contours[max_index]
+        yuv = self.picam2.capture_array("lores")
+        image = cv2.cvtColor(yuv, cv2.COLOR_YUV420p2RGB)[0:450, 100:540]
 
-                sum_dist = 0
-                for p in self.points:
-                    x_value = 0
-                    y_value = p[1]
-                    i = 0
-                    for point in cnt:
-                        if point[0][1] == y_value:
-                            x_value = point[0][0] + x_value
-                            i = i +1
-                    if not i == 0:
-                        sum_dist = sum_dist + x_value/i - p[0]
-                        cv2.circle(img2, p, radius=5, color=(0, 0, 255), thickness=-1)
-                        cv2.circle(img2, (int(x_value/i), y_value), radius=5, color=(255, 0, 0), thickness=-1)
-                self.y = sum_dist/len(self.points)
-
-                # recognizing 90 turn
-                if cv2.pointPolygonTest(cnt, self.leftT, False) >= 0 and cv2.pointPolygonTest(cnt, self.rightT, False) < 0 :
-                    turnOfset = -self.turnOffsetParam#-1.0 #-1
-                elif cv2.pointPolygonTest(cnt, self.leftT, False) < 0 and cv2.pointPolygonTest(cnt, self.rightT, False) >= 0 :
-                    turnOfset = self.turnOffsetParam#1.0 #1
-            u = self.pid.pid(self.y,0) + turnOfset
-
-            if u > self.maxU:
-                u = self.maxU
-
-            if u < self.minU:
-                u = self.minU
-
-
-            # publishing linear and angular robot speed
-            cmd = Twist()
-            cmd.linear.x = self.speed
+        # publishing linear and angular robot speed
+        cmd = Twist()
+        cmd.linear.x = self.speed
+        self.line_follower.set_image(image, LineFollowerModule.InputMode.RAW)
+        u = self.line_follower.compute_angular_velocity()
+        if u is not None:
             cmd.angular.z = u
-            self.publisher3_.publish(cmd)
-            if not np.size(areas) == 0:
-                cv2.drawContours(image=img2, contours=[cnt], contourIdx=-1, color=(0, 255, 0), thickness=3, lineType=cv2.LINE_AA)
-            msg = br.cv2_to_imgmsg(img2)
+        self.publisher3_.publish(cmd)
+
+        try:
+            msg = self.bridge.cv2_to_imgmsg(self.line_follower.get_image_binary())
+            self.publisher1_.publish(msg)
+        except LineFollowerModuleError as e:
+            self.logger.info(f'Failed to obtain binary image, the error is \"{e}\"')
+
+        try:
+            msg = self.bridge.cv2_to_imgmsg(self.line_follower.get_image_debug())
             self.publisher2_.publish(msg)
+        except LineFollowerModuleError as e:
+            self.logger.info(f'Failed to obtain debug image, the error is \"{e}\"')
 
     def subscr_callback(self, msg):
         self.is_working = msg.data
-
-
 
 def main(args=None):
     rclpy.init(args=args)
--- followers/py_scripts/linefollower_no_cam.py
+++ followers/py_scripts/linefollower_no_cam.py
@@ -2,24 +2,15 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-import cv2
 from cv_bridge import CvBridge
-import numpy as np
 from rclpy.parameter import Parameter
-from pid import PID
 from std_msgs.msg import Bool
 from geometry_msgs.msg import Twist
+from linefollower_module import LineFollowerModule, LineFollowerModuleError
 
 class LineFollowerNoCamm(Node):
-    img = None
-    pid = None
-    #minU = -5.14
-    #maxU = 5.14
-    #points = [(220, 270),(220, 230),(220, 200),(220, 170)]
-    points = [(220, 200)]
-    leftT = (435, 270)
-    rightT = (5, 270)
-    y = 0
+    bridge = CvBridge()
+    is_working = True
 
     def __init__(self):
         super().__init__('line_follower_no_cam')
@@ -49,78 +40,41 @@ class LineFollowerNoCamm(Node):
         self.logger.info(f"timer_period: { timer_period}, maxU: {self.maxU}, K: {K}, Ti:{Ti}")
         self.logger.info(f"Td: {Td},turnOffsetParam: {self.turnOffsetParam}, speed: {self.speed}, namespace:{self.get_namespace()}")
 
+        self.line_follower = LineFollowerModule(timer_period, K, Ti, Td, self.minU, self.maxU, self.turnOffsetParam, self.get_logger())
+
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.thresh = None
 
         self.subscription = self.create_subscription(Image, '/cv_video_frames',
             self.listener_callback, 10)
 
-        self.pid = PID(timer_period,K,Ti,Td)
-
         self.publisher3_ = self.create_publisher(Twist, 'cmd_vel', 10)
         self.subscriber = self.create_subscription(Bool, 'is_line_follower', self.subscr_callback, 10)
-
-        self.is_working = True
-
+        self.publisher2_ = self.create_publisher(Image, 'mod_img', 10)
 
     def listener_callback(self, msg):
-        br = CvBridge()
-        self.thresh = br.imgmsg_to_cv2(msg)
+        thresh = self.bridge.imgmsg_to_cv2(msg)
+        self.line_follower.set_image(thresh, LineFollowerModule.InputMode.PROCESSED)
 
     def timer_callback(self):
-        if self.thresh is not None and self.is_working:
-            thresh = self.thresh
-            # contours
-            contours, hierarchy = cv2.findContours(image=thresh, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_NONE)
-            img2 = self.img
-            # biggest contour
-            areas = [cv2.contourArea(c) for c in contours]
+        if not self.is_working:
+            return
 
-            # getting position where robot should be
-            turnOfset = 0
-            if not np.size(areas) == 0:
-                max_index = np.argmax(areas)
-                cnt=contours[max_index]
-
-                sum_dist = 0
-                for p in self.points:
-                    x_value = 0
-                    y_value = p[1]
-                    i = 0
-                    for point in cnt:
-                        if point[0][1] == y_value:
-                            x_value = point[0][0] + x_value
-                            i = i +1
-                    if not i == 0:
-                        sum_dist = sum_dist + x_value/i - p[0]
-                        cv2.circle(img2, p, radius=5, color=(0, 0, 255), thickness=-1)
-                        cv2.circle(img2, (int(x_value/i), y_value), radius=5, color=(255, 0, 0), thickness=-1)
-                self.y = sum_dist/len(self.points)
-
-                # recognizing 90 turn
-                if cv2.pointPolygonTest(cnt, self.leftT, False) >= 0 and cv2.pointPolygonTest(cnt, self.rightT, False) < 0 :
-                    print("prawy")
-                    turnOfset = -self.turnOffsetParam #-1
-                elif cv2.pointPolygonTest(cnt, self.leftT, False) < 0 and cv2.pointPolygonTest(cnt, self.rightT, False) >= 0 :
-                    print("lewy")
-                    turnOfset = self.turnOffsetParam #1
-            u = self.pid.pid(self.y,0) + turnOfset
-            print(turnOfset)
-            if u > self.maxU:
-                u = self.maxU
-
-            if u < self.minU:
-                u = self.minU
-
-            # publikowanie u
-            cmd = Twist()
-            cmd.linear.x = self.speed
+        # publishing linear and angular robot speed
+        cmd = Twist()
+        cmd.linear.x = self.speed
+        u = self.line_follower.compute_angular_velocity()
+        if u is not None:
             cmd.angular.z = u
-            self.publisher3_.publish(cmd)
+        self.publisher3_.publish(cmd)
+
+        try:
+            msg = self.bridge.cv2_to_imgmsg(self.line_follower.get_image_debug())
+            self.publisher2_.publish(msg)
+        except LineFollowerModuleError as e:
+            self.logger.info(f'Failed to obtain debug image, the error is \"{e}\"')
 
     def subscr_callback(self, msg):
         self.is_working = msg.data
-
 
 def main(args=None):
     rclpy.init(args=args)
